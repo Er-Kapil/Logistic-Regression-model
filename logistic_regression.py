d22a27a3e923f9b760c3d1e5ccf3145cc80730a1
Er-Kapil <157969010+Er-Kapil@users.noreply.github.com>
2025-09-05 18:57:37 +0530

    Initial Commit

---
+++ logistic_regression.py
@@ -0,0 +1,14 @@
+import numpy as np
+class logistic_regression:
+    # declaring the learning rate and the number of iterations (hyperparameters)
+    def __init__(self,learning_rate,epochs):
+        self.learning_rate=learning_rate
+        self.epochs=epochs
+    
+    # fit function to train the model with some dataset
+    def fit(self):
+        pass
+    def update_weights(self):
+        pass
+    def predict(self):
+        pass
