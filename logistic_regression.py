0671683db30a2b954666859ca092825f916c95e1
Er-Kapil <157969010+Er-Kapil@users.noreply.github.com>
2025-09-05 23:34:52 +0530

    Implement fit method in logistic_regression class to initialize training parameters and prepare for gradient descent

--- logistic_regression.py
+++ logistic_regression.py
@@ -6,8 +6,20 @@ class logistic_regression:
         self.epochs=epochs
     
     # fit function to train the model with some dataset
-    def fit(self):
-        pass
+    def fit(self,X,Y):
+        # number of datapoints in the dataset ----> m
+        # Number of input features in the dataset ----> n
+        self.m,self.n = X.shape
+
+        # Initiating the weight value and bias value
+        self.w=np.zeros(self.n)
+        self.b=0
+        self.x=X
+        self.y=Y
+
+        # Implementing gradient descent
+        for i in range(epochs):
+            self.update_weights()
     def update_weights(self):
         pass
     def predict(self):
